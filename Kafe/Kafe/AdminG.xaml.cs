51920aedb388b016d56527cc1652de10be6364fe
bersik-dot <bersik0@mail.ru>
2023-12-19 20:35:11 +0300

    1

---
+++ Kafe/Kafe/AdminG.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kafe
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminG.xaml
+    /// </summary>
+    public partial class AdminG : Page
+    {
+        public AdminG()
+        {
+            InitializeComponent();
+        }
+
+        private void dob_sot_Click(object sender, RoutedEventArgs e)
+        {
+            Frameobj.frameMain.Navigate(new AdminDobSot());
+        }
+    }
+}
